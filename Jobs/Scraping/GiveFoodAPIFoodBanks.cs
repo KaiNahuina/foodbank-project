257395230d6aadaa3b05b458565511bb399a0ef4
Dylan Noble <2012667@chester.ac.uk>
2022-04-28 16:49:34 +0100

    Cleanup Give Food API job

--- Jobs/Scraping/GiveFoodAPIFoodBanks.cs
+++ Jobs/Scraping/GiveFoodAPIFoodBanks.cs
@@ -13,13 +13,13 @@ namespace Foodbank_Project.Jobs.Scraping
         {
             _logger = logger;
         }
-
-        // TODO: Cleanup
         public async Task Execute(IJobExecutionContext context)
         {
-            try
+            _logger.LogInformation("Commencing GiveFood API Scraping job...");
+            Stopwatch stopwatch = new Stopwatch();
+            List<Foodbank> foodbanks;
+            // Stage 1
             {
-                _logger.LogInformation("Give Food API Scrapper Job Started");
                 HttpClient client = new HttpClient();
                 client.BaseAddress = new Uri("https://www.givefood.org.uk/api/2/foodbanks/");
                 client.DefaultRequestHeaders.Accept.Clear();
@@ -27,80 +27,78 @@ namespace Foodbank_Project.Jobs.Scraping
                     new MediaTypeWithQualityHeaderValue("application/json")
                 );
 
-
-                var sw = new Stopwatch();
-                HttpResponseMessage response = await client.GetAsync("");
-                if (response.IsSuccessStatusCode)
+                try
                 {
-                    List<Foodbank> foodbanks = await response.Content.ReadAsAsync<List<Foodbank>>();
-                    sw.Stop();
-                    _logger.LogInformation($"Successfully gathered {foodbanks.Count} foodbanks in {sw.ElapsedMilliseconds}ms");
-
-                    HttpClient subClient = new HttpClient();
-                    for (int i = 0; i < foodbanks.Count; i++)
+                    stopwatch.Start();
+                    HttpResponseMessage response = await client.GetAsync("");
+                    if (response.IsSuccessStatusCode)
                     {
-                        var _foodbank = foodbanks[i];
-                        if (_foodbank.Urls is not null && _foodbank.Urls.Self is not null)
+                        stopwatch.Stop();
+                        foodbanks = await response.Content.ReadAsAsync<List<Foodbank>>();
+                        _logger.LogInformation($"Finished gathering foodbank centres. Got {foodbanks.Count} in {stopwatch.ElapsedMilliseconds}ms");
+                        response.Dispose();
+                        client.Dispose();
+                    }
+                    else
+                    {
+                        response.Dispose();
+                        client.Dispose();
+                        throw new Exception($"HttpClient responded with non 200 code: {response.StatusCode} - {response.ReasonPhrase}");
+                    }
+                }catch (Exception ex)
+                {
+                    client.Dispose();
+                    _logger.LogError($"Something went really wrong! Job will be skipped! {ex}");
+                    return;
+                }
+            }
+            // Stage 2
+            {
+                HttpClient client = new HttpClient();
+                HttpRequestMessage request;
+                Foodbank foodbank;
+                for (int i = 0; i < foodbanks.Count; i++)
+                {
+                    foodbank = foodbanks[i];
+                    if (foodbank.Urls is not null && foodbank.Urls.Self is not null)
+                    {
+                        request = new HttpRequestMessage(HttpMethod.Get, foodbank.Urls.Self);
+                        request.Headers.Accept.Clear();
+                        request.Headers.Accept.Add(
+                            new MediaTypeWithQualityHeaderValue("application/json")
+                        );
+
+                        try
                         {
-                            var request = new HttpRequestMessage(HttpMethod.Get, _foodbank.Urls.Self);
-                            request.Headers.Accept.Clear();
-                            request.Headers.Accept.Add(
-                                new MediaTypeWithQualityHeaderValue("application/json")
-                            );
-                            sw.Restart();
-                            HttpResponseMessage subResponse;
-                            try
+                            stopwatch.Restart();
+                            HttpResponseMessage response = await client.SendAsync(request);
+                            if (response.IsSuccessStatusCode)
                             {
-                                subResponse = await subClient.SendAsync(request);
-                            }
-                            catch (Exception ex)
-                            {
-                                _logger.LogError($"Something went very wrong! {ex.ToString()}. Skipping...");
-                                continue;
-                            }
-                            if (subResponse.IsSuccessStatusCode)
-                            {
-                                foodbanks[i].Merge(await subResponse.Content.ReadAsAsync<Foodbank>());
-                                sw.Stop();
-                                _logger.LogInformation($"Succesfully added locations for {_foodbank.Name}({_foodbank.Urls.Self}) in {sw.ElapsedMilliseconds}ms [{i + 1} of {foodbanks.Count}]");
-                                Thread.Sleep(200);
+                                foodbanks[i].Merge(await response.Content.ReadAsAsync<Foodbank>());
+                                stopwatch.Stop();
+                                _logger.LogInformation($"Succesfully added locations for {foodbank.Name}({foodbank.Urls.Self}) in {stopwatch.ElapsedMilliseconds}ms [{i + 1} of {foodbanks.Count}]");
+                                Thread.Sleep(100);
                             }
                             else
                             {
-                                _logger.LogError($"HTTP ERROR: {(int)subResponse.StatusCode} ({subResponse.ReasonPhrase}). Skipping...");
+                                response.Dispose();
+                                request.Dispose();
+                                throw new Exception($"HttpClient responded with non 200 code: {response.StatusCode} - {response.ReasonPhrase}");
                             }
                         }
-                        else
+                        catch (Exception ex)
                         {
-                            _logger.LogError($"Something went really wrong! Skipping {foodbanks[i].Name ?? "NAME MISSING"}. Skipping...");
+                            request.Dispose();
+                            _logger.LogError($"Something went really wrong! Skipping... {ex}");
                             continue;
-
                         }
 
                     }
-                    subClient.Dispose();
-                    {
-                        var count = 0;
-                        foreach (var item in foodbanks)
-                        {
-                            count += item.Locations?.Count ?? 0;
-                        }
-                        _logger.LogInformation($"Successfully gathered {count} foodbank locations");
-                    }
-                    // TODO: Stuff with foodbanks
                 }
-                else
-                {
-                    _logger.LogError($"HTTP ERROR: {(int)response.StatusCode} ({response.ReasonPhrase}). Job aborted");
-                }
-
                 client.Dispose();
+            }
+            _logger.LogInformation("GiveFood API Scraping job finished");
 
-            }
-            catch (Exception ex)
-            {
-                _logger.LogError($"Something went very wrong! {ex.ToString()}. Job aborted");
-            }
         }
     }
 
