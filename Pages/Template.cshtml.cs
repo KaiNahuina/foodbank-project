415e66140a21e99623e31b1524741c30ea51b52f
JACK BURNISTON <2004160@chester.ac.uk>
2022-05-05 20:22:13 +0100

    Added a few more options and created some more options for templates

---
+++ Pages/Template.cshtml.cs
@@ -0,0 +1,14 @@
+﻿#region
+
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+#endregion
+
+namespace Foodbank_Project.Pages;
+
+public class TemplateModel : PageModel
+{
+    public void OnGet()
+    {
+    }
+}
