f88abe1141d49321b1ae8cf3eea24f2fc0375861
Dylan Noble <2012667@chester.ac.uk>
2022-05-12 14:02:34 +0100

    Fixed AJAX response for map

--- Pages/testMap.cshtml.cs
+++ Pages/testMap.cshtml.cs
@@ -29,14 +29,26 @@ namespace Foodbank_Project.Pages
             Locations = await locations.AsNoTracking().ToListAsync();
         }
 
+        // DTO
         public class Coords { public double Lat { get; set; } public double Lng { get; set; } }
-        public async Task<IActionResult> OnPostCoordAsync([FromBody] Coords obj)
+        public async Task<JsonResult> OnPostCoordAsync([FromBody] Coords obj)
         {
             var origin = new NetTopologySuite.Geometries.Point(obj.Lng, obj.Lat) { SRID = 4326 };
 
             var foodBankLocations = await _ctx
                 .Locations.AsNoTracking()
-                .Select(l => new {place = l, Distance = l.Coord.Distance(origin) }).ToListAsync();
+                .Select(l => new
+                {
+                    Distance = l.Coord.Distance(origin),
+                    Name = l.Name,
+                    Id = l.LocationId,
+                    Address = l.Address,
+                    Coord = new Coords
+                    {
+                        Lat = l.Coord.Y,
+                        Lng = l.Coord.X
+                    }
+                }).ToListAsync();
 
             var top5Locations = foodBankLocations.OrderBy(l => l.Distance).Take(5).ToList();
 
