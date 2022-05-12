45637af9971cf34702e645592e10ddadc3257c32
STEPHEN LOMAX <2020062@chester.ac.uk>
2022-05-12 17:11:42 +0100

    Steve add give help pages and CSS and Layout changes

---
+++ Pages/Voucher System.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Foodbank_Project.Pages
+{
+    public class Voucher_SystemModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
