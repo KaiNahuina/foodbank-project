0b064bbbfd962f42b6ba7a6d396430357281476f
Dylan Noble <2012667@chester.ac.uk>
2022-05-13 15:35:44 +0100

    Added sort and filtering to Admin pages

---
+++ Pages/Admin/Needs.cshtml.cs
@@ -0,0 +1,88 @@
+﻿#region
+
+using Foodbank_Project.Data;
+using Foodbank_Project.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Build.Framework;
+using Microsoft.EntityFrameworkCore;
+
+#endregion
+
+namespace Foodbank_Project.Pages.Admin;
+
+public class StockModel : PageModel
+{
+    private readonly ApplicationContext _ctx;
+
+    public IList<Need> Needs;
+
+
+    public string OrderDirection;
+    public string OrderBy;
+    public int Page;
+
+    public StockModel(ApplicationContext ctx)
+    {
+        _ctx = ctx;
+    }
+
+    public async Task OnGetAsync([FromQuery(Name = "OrderBy")]string orderBy,
+        [FromQuery(Name = "OrderDirection")]string orderDirection,
+        [FromQuery(Name = "Search")]string search, [FromQuery(Name = "Page")]string page)
+    {
+        OrderBy = string.IsNullOrEmpty(orderBy) ? "Count" : orderBy;
+        OrderDirection = string.IsNullOrEmpty(orderDirection) ? "Desc" : orderDirection;
+        Page = string.IsNullOrEmpty(page) && int.TryParse(page, out Page) ? 1 : int.Parse(page);
+
+        var needQue = (from f in _ctx.Needs
+            select f).AsNoTracking()
+            .Include(n => n.Foodbanks)
+            .OrderByDescending(n => n.Foodbanks.Count)
+            .Where(n => n.NeedStr != null).Where(n => string.IsNullOrEmpty(search) || n.NeedStr.Contains(search) || n.NeedId.Equals(search));
+
+        // do sort and filter shizzle here
+
+        switch (OrderDirection)
+        {
+            case "Asc":
+            {
+                switch (OrderBy)
+                {
+                    case "Name":
+                    {
+                        needQue = needQue.OrderBy(n => n.NeedStr);
+                        break;
+                    }
+                    case "Count":
+                    {
+                        needQue = needQue.OrderBy(n => n.Foodbanks.Count);
+                        break;
+                    }
+                }
+
+                break;
+            }
+            case "Desc":
+            {
+                switch (OrderBy)
+                {
+                    case "Name":
+                    {
+                        needQue = needQue.OrderByDescending(n => n.NeedStr);
+                        break;
+                    }
+                    case "Count":
+                    {
+                        needQue = needQue.OrderByDescending(n => n.Foodbanks.Count);
+                        break;
+                    }
+                }
+
+                break;
+            }
+        }
+
+        Needs = await needQue.Take(25).ToListAsync();
+    }
+}
