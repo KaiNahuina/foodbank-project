1f94332b20c1bfa3b99aeee2b1fd3ee290aa68a4
JUAN ZUMARAN <2007480@chester.ac.uk>
2022-05-16 17:01:39 +0100

    Added first iteration of the edit foodbank page. Second iteration of the add foodbank page removed opening times and needs. Did some of the back end for both pages (untested)

--- Pages/AddBank.cshtml.cs
+++ Pages/AddBank.cshtml.cs
@@ -1,5 +1,8 @@
 #region
 
+using Foodbank_Project.Data;
+using Foodbank_Project.Models;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 #endregion
@@ -8,6 +11,51 @@ namespace Foodbank_Project.Pages;
 
 public class AddBankModel : PageModel
 {
+
+    private ApplicationContext _ap;
+
+    public Foodbank foodbank { get; set; }
+
+    private Need need { get; set; }
+
+    public string needs;
+
+    public string add1;
+
+    public string? add2;
+
+    public string city;
+
+    public string? region;
+
+    private string fullAddress;
+
+    public string opening;
+
+    public string closing;
+
+    private Tuple<string> openingTimes;
+
+
+    public AddBankModel(ApplicationContext ap)
+    {
+        _ap = ap;
+    }
+
+    public async Task<IActionResult> OnPost()
+    {
+        if(!ModelState.IsValid)
+        {
+            return Page();
+        }
+        fullAddress = $"{add1} {add2} {city} {region}";
+        foodbank.Address = fullAddress;
+        foodbank.Closed = false;
+        foodbank.Status = Status.UnConfirmed;
+        _ap.Foodbanks.Add(foodbank);
+        await _ap.SaveChangesAsync();
+        return RedirectToPage("/Index");
+    }
     public void OnGet()
     {
     }
