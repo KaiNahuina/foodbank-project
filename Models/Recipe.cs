68f38bebb4f682f0c4e0a76b2fa6dd90c1b849e7
STEPHEN LOMAX <2020062@chester.ac.uk>
2022-05-17 18:11:49 +0100

    added notes and serves to  add recipe form

--- Models/Recipe.cs
+++ Models/Recipe.cs
@@ -5,10 +5,11 @@ public class Recipe
     public int RecipeId { get; set; }
 
     public string Name { get; set; }
-
-    public string Ingredients { get; set; }
+    public string Serves { get; set; }
+    public string? Ingredients { get; set; }
     public string Method { get; set; }
-    public byte[] Image { get; set; }
+    public string? Notes { get; set; }
+    public byte[]? Image { get; set; }
 
     public RecipeCategory? Category { get; set; }
 }
