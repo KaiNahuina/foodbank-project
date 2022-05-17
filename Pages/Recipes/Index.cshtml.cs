e0a18730fe7752348453d2997a44f2836289e09c
JACK BURNISTON <2004160@chester.ac.uk>
2022-05-17 11:01:09 +0100

    Further cleaning and oganisation of Recipes

--- Pages/Recipes/Index.cshtml.cs
+++ Pages/Recipes/Index.cshtml.cs
@@ -4,9 +4,9 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 #endregion
 
-namespace Foodbank_Project.Pages;
+namespace Foodbank_Project.Pages.Recipes.IndexModel;
 
-public class RecipesModel : PageModel
+public class IndexModel : PageModel
 {
     public void OnGet()
     {
