aba9014620481799b007b9545d99a788a8079887
rishabh garg <rishabgarg21101998@gmail.com>
2022-05-20 11:35:10 +0100

    thankyou

---
+++ Pages/Recipes/thankyou.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Foodbank_Project.Pages.Recipes
+{
+    public class thankyouModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
