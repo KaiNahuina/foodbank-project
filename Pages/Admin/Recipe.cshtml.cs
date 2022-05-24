bb3793481222a13f904c491c9fc9c87c632b8a38
Dylan Noble <2012667@chester.ac.uk>
2022-05-24 13:43:31 +0100

    Corrected validation. Added Recipe page. Added recipe category page

---
+++ Pages/Admin/Recipe.cshtml.cs
@@ -0,0 +1,157 @@
+#region
+
+using Foodbank_Project.Data;
+using Foodbank_Project.Models;
+using Foodbank_Project.Util;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using NetTopologySuite.Geometries;
+
+#endregion
+
+namespace Foodbank_Project.Pages.Admin;
+
+public class RecipeModel : PageModel
+{
+    private readonly ApplicationContext _ctx;
+
+    public string? Action;
+    
+    public bool HasNextPage;
+    public bool HasPrevPage;
+    public int MaxPages;
+    public string? OrderBy;
+    public string? OrderDirection;
+    public new int Page;
+    public string? Search;
+    public int TotalItems;
+    
+    public IList<Models.RecipeCategory>? Categories;
+
+    public RecipeModel(ApplicationContext ctx)
+    {
+        _ctx = ctx;
+    }
+
+    [BindProperty] public Recipe? Recipe { get; set; }
+    
+    [BindProperty] public IFormFile? Upload { get; set; }
+
+    public async Task OnGetAsync([FromQuery(Name = "Action")] string? action, [FromQuery(Name = "OrderBy")] string? orderBy,
+        [FromQuery(Name = "OrderDirection")] string? orderDirection,
+        [FromQuery(Name = "Search")] string? search, [FromQuery(Name = "Page")] string? page)
+    {
+        Action = action ?? "Update";
+        if (Action != "Create")
+        {
+            var id = int.Parse(RouteData.Values["id"] as string ?? "");
+            OrderBy = string.IsNullOrEmpty(orderBy) ? "Name" : orderBy;
+            OrderDirection = string.IsNullOrEmpty(orderDirection) ? "Desc" : orderDirection;
+            if (!int.TryParse(page, out Page)) Page = 1;
+            if (string.IsNullOrEmpty(search))
+            {
+                if (string.IsNullOrEmpty(Search)) Search = "";
+            }
+            else
+            {
+                Search = search;
+            }
+            
+            var recipeQue = from f in _ctx.Recipes where f.RecipeId == id select f;
+
+            var categoryQue = _ctx.RecipeCategories!.AsNoTracking().Include(r => r.Recipes
+                    .Where(c => c.RecipeId == id))
+                .OrderByDescending(n => n.Name)
+                .Where(n => n.Recipes.Any(r => r.RecipeId == id) &&
+                    string.IsNullOrEmpty(Search) || n.Name!.Contains(Search)
+                    || n.RecipeCategoryId.ToString() == Search);
+            
+            
+            switch (OrderDirection)
+            {
+                case "Asc":
+                {
+                    categoryQue = OrderBy switch
+                    {
+                        "Name" => categoryQue.OrderBy(n => n.Name),
+                        _ => categoryQue
+                    };
+                    break;
+                }
+                case "Desc":
+                {
+                    categoryQue = OrderBy switch
+                    {
+                        "Name" => categoryQue.OrderByDescending(n => n.Name),
+                        _ => categoryQue
+                    };
+                    break;
+                }
+            }
+            
+            HasPrevPage = Page > 1;
+
+            TotalItems = await categoryQue.CountAsync();
+            MaxPages = (int)Math.Ceiling(TotalItems / 25d);
+
+            HasNextPage = Page < MaxPages;
+
+            Categories = await categoryQue.Skip((Page - 1) * 25).Take(25).ToListAsync();
+
+            Recipe = await recipeQue.AsNoTracking().Include(l => l.Category).FirstAsync();
+        }
+        else
+        {
+            Recipe = new Recipe();
+        }
+    }
+
+
+    public async Task<IActionResult> OnPostAsync([FromQuery(Name = "Action")] string? action)
+    {
+        Action = action ?? Request.Form["Action"].ToString() ?? "Update";
+        switch (Action)
+        {
+            case "Delete":
+                if (Recipe != null) _ctx.Remove(Recipe);
+                break;
+            case "Create":
+            {
+                if (!ModelState.IsValid) return Page();
+                if (Recipe != null)
+                {
+                    if (Upload is not null)
+                    {
+                        MemoryStream ms = new MemoryStream();
+                        await Upload.CopyToAsync(ms);
+                        Recipe.Image = ms.ToArray();
+                    }
+                    _ctx.Recipes?.Update(Recipe);
+                }
+
+                break;
+            }
+            case "Update":
+            {
+                if (!ModelState.IsValid) return Page();
+                if (Recipe != null)
+                {
+                    if (Upload is not null)
+                    {
+                        MemoryStream ms = new MemoryStream();
+                        await Upload.CopyToAsync(ms);
+                        Recipe.Image = ms.ToArray();
+                    }
+                    _ctx.Recipes?.Update(Recipe);
+                }
+
+                break;
+            }
+
+        }
+
+        await _ctx.SaveChangesAsync();
+        return RedirectToPage("./Recipes");
+    }
+}
--- Pages/Admin/Recipes.cshtml.cs
+++ Pages/Admin/Recipes.cshtml.cs
@@ -1,10 +1,103 @@
 #region
 
+using Foodbank_Project.Data;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 
 #endregion
 
 namespace Foodbank_Project.Pages.Admin;
 
 public class RecipesModel : PageModel
-{}
+{
+    
+    private readonly ApplicationContext _ctx;
+
+    public IList<Models.Recipe>? Recipes;
+
+    public bool HasNextPage;
+    public bool HasPrevPage;
+    public int MaxPages;
+    public string? OrderBy;
+    public string? OrderDirection;
+    public new int Page;
+    public string? Search;
+    public int TotalItems;
+
+    public RecipesModel(ApplicationContext ctx)
+    {
+        _ctx = ctx;
+    }
+    
+    public async Task OnGetAsync([FromQuery(Name = "OrderBy")] string? orderBy,
+        [FromQuery(Name = "OrderDirection")] string? orderDirection,
+        [FromQuery(Name = "Search")] string? search, [FromQuery(Name = "Page")] string? page)
+    {
+        OrderBy = string.IsNullOrEmpty(orderBy) ? "Name" : orderBy;
+        OrderDirection = string.IsNullOrEmpty(orderDirection) ? "Desc" : orderDirection;
+        if (!int.TryParse(page, out Page)) Page = 1;
+        if (string.IsNullOrEmpty(search))
+        {
+            if (string.IsNullOrEmpty(Search)) Search = "";
+        }
+        else
+        {
+            Search = search;
+        }
+
+        var recipeQue = (from f in _ctx.Recipes
+                select f).AsNoTracking()
+            .OrderByDescending(n => n.Name)
+            .Where(n =>
+                string.IsNullOrEmpty(Search) || n.Name!.Contains(Search) || n.Notes!.Contains(Search) ||
+                n.Method!.Contains(Search)
+                || n.Ingredients! == Search);
+
+        switch (OrderDirection)
+        {
+            case "Asc":
+            {
+                recipeQue = OrderBy switch
+                {
+                    "Name" => recipeQue.OrderBy(n => n.Name),
+                    "Method" => recipeQue.OrderBy(n => n.Method),
+                    "Ingredients" => recipeQue.OrderBy(n => n.Ingredients),
+                    "Notes" => recipeQue.OrderBy(n => n.Notes),
+                    _ => recipeQue
+                };
+
+                break;
+            }
+            case "Desc":
+            {
+                recipeQue = OrderBy switch
+                {
+                    "Name" => recipeQue.OrderByDescending(n => n.Name),
+                    "Method" => recipeQue.OrderByDescending(n => n.Method),
+                    "Ingredients" => recipeQue.OrderByDescending(n => n.Ingredients),
+                    "Notes" => recipeQue.OrderByDescending(n => n.Notes),
+                    _ => recipeQue
+                };
+
+                break;
+            }
+        }
+
+        HasPrevPage = Page > 1;
+
+        TotalItems = await recipeQue.CountAsync();
+        MaxPages = (int)Math.Ceiling(TotalItems / 25d);
+
+        HasNextPage = Page < MaxPages;
+
+        Recipes = await recipeQue.Skip((Page - 1) * 25).Take(25).ToListAsync();
+    }
+    public string TrimBlob(string? blob)
+    {
+        if (blob is null) return "";
+        if (blob.Length > 32)
+            return blob[..32] + " ...";
+        return blob;
+    }
+}
